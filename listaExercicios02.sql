70515c8836f287fe67ec682e2e6d964ddb20b8ca
Matheus <omatheusphilipe@gmail.com>
2023-09-26 20:13:43 -0300

    7º

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -59,6 +59,13 @@ BEGIN
 END//
 
 DELIMITER ;
+DECLARE @possuiLivros BOOL;
+CALL sp_VerificarLivrosCategoria('Romance', @possuiLivros);
+IF @possuiLivros THEN
+    SELECT 'A categoria possui livros.' AS Resultado;
+ELSE
+    SELECT 'A categoria não possui livros.' AS Resultado;
+END IF;
 
 --5
 DELIMITER //
@@ -90,3 +97,38 @@ DELIMITER ;
 CALL sp_TitulosPorCategoria('Romance');
 CALL sp_TitulosPorCategoria('Ciência');
 CALL sp_TitulosPorCategoria('Ficção Científica');
+
+--7
+DELIMITER //
+CREATE PROCEDURE sp_AdicionarLivro(
+    IN tituloLivro VARCHAR(255),
+    IN editoraID INT,
+    IN anoPublicacao INT,
+    IN numeroPaginas INT,
+    IN categoriaID INT,
+    OUT resultado VARCHAR(255)
+)
+BEGIN
+    DECLARE livroExiste INT;
+    
+    -- Verificar se o livro já existe com o mesmo título
+    SELECT COUNT(*) INTO livroExiste
+    FROM Livro
+    WHERE Titulo = tituloLivro;
+    
+    -- Se o livro já existe, retornar uma mensagem de erro
+    IF livroExiste > 0 THEN
+        SET resultado = 'Erro: Já existe um livro com esse título.';
+    ELSE
+        -- Inserir o novo livro na tabela Livro
+        INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+        VALUES (tituloLivro, editoraID, anoPublicacao, numeroPaginas, categoriaID);
+        
+        SET resultado = 'Livro adicionado com sucesso.';
+    END IF;
+END//
+
+DELIMITER ;
+DECLARE @resultado VARCHAR(255);
+CALL sp_AdicionarLivro('Novo Livro', 1, 2023, 300, 2, @resultado);
+SELECT @resultado AS Resultado;
