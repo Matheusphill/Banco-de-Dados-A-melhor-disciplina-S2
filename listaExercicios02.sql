3bd11dc2fa32d62cfe314ca88466c0f07c3ea1f2
Matheus <omatheusphilipe@gmail.com>
2023-09-26 19:55:07 -0300

    2º

---
+++ listaExercicios02.sql
@@ -0,0 +1,26 @@
+DELIMITER //
+
+CREATE PROCEDURE sp_ListarAutores()
+BEGIN
+    SELECT * FROM Autor;
+END//
+
+DELIMITER ;
+CALL sp_ListarAutores();
+
+--2
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome AS NomeAutor, Autor.Sobrenome AS SobrenomeAutor
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = categoriaNome;
+END//
+
+DELIMITER ;
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Ciência');
+CALL sp_LivrosPorCategoria('Ficção Científica');
